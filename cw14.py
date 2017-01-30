09ca8755cbd88f83b039edcbd4293498f798e64c
Gehr <TEVIS.GEHR@accudyneindustries.com>
2017-01-30 15:54:20 -0700

    is_pointer1_at_lowest_digit defined

--- cw14.py
+++ cw14.py
@@ -1,19 +1,34 @@
+'''
+cd '.\Python\TevisPrograms\Code Wars'
+python
+import cw14 as cw14
+cw14.next_bigger(1243)
+'''
 class NextHighestAnagram(object):
 
     def go(self, n):
         n_list = list(str(n))
         print n_list
-        if (self.is_sorted_original()):
-            print "is_sorted_original:", self.is_sorted_original()
+        print "is_sorted_original:", self.is_sorted_original(n_list)
 
-    def is_sorted_original(self):
+        if (self.is_sorted_original(n_list)):
+            return -1
+
+        pointer1 = 1
+        pointer2 = 2
+
+        print "is_pointer1_at_lowest_digit:", self.is_pointer1_at_lowest_digit(pointer1)
+        if self.is_pointer1_at_lowest_digit(pointer1):
+            pass
+
+    def is_sorted_original(self,n_list):
         if (sorted(n_list) == n_list):
             return True
         else:
             return False
 
-    def is_pointer1_at_lowest_digit(self):
-        pass
+    def is_pointer1_at_lowest_digit(self, pointer1):
+        return pointer1 == 1
 
     def is_next_digit_lesser_value__pointer2(self):
         pass
@@ -36,4 +51,4 @@ class NextHighestAnagram(object):
 
 def next_bigger(n):
     nha = NextHighestAnagram()
-    nha.go(n)
+    return nha.go(n)
