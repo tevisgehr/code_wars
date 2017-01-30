3ee9c87541d832041ff432766a3267926da46284
Gehr <TEVIS.GEHR@accudyneindustries.com>
2017-01-30 15:11:49 -0700

    getting rid of init method, will pass n directly to 'go' method

--- cw14.py
+++ cw14.py
@@ -1,33 +1,32 @@
 class NextHighestAnagram(object):
-    def __init__(self, n):
-        self.n = n
 
     def go(self):
         pass
-    def is_sorted_original():
+
+    def is_sorted_original(self):
         pass
 
-    def is_pointer1_at_lowest_digit():
+    def is_pointer1_at_lowest_digit(self):
         pass
 
-    def is_next_digit_lesser_value__pointer2():
+    def is_next_digit_lesser_value__pointer2(self):
         pass
 
-    def is_pointer2_on_highest_digit():
+    def is_pointer2_on_highest_digit(self):
         pass
 
-    def move_pointer2_up_one_digit():
+    def move_pointer2_up_one_digit(self):
         pass
 
-    def move_pointer1_up_one_digit__pointer2_one_higher():
+    def move_pointer1_up_one_digit__pointer2_one_higher(self):
         pass
 
-    def swap_split_sort_combine():
+    def swap_split_sort_combine(self):
         pass
 
 
 
 
 def next_bigger(n):
-    nha = NextHighestAnagram(n)
+    nha = NextHighestAnagram()
     nha.go(n)
