2caf9050df23603c2145a6013fcd0683a8dc6c5e
Gehr <TEVIS.GEHR@accudyneindustries.com>
2017-01-31 11:16:53 -0700

    macro structure changes to make it return value correctly and comment out print statments

--- cw14.py
+++ cw14.py
@@ -2,17 +2,17 @@
 cd '.\Python\TevisPrograms\Code Wars'
 python
 import cw14 as cw14
-cw14.next_bigger(71723472953999999999999999)
+cw14.next_bigger(78543987654321)
 '''
 class NextHighestAnagram(object):
 
     def go(self, n):
         n_list = list(str(n))
-        print n_list
+        #print n_list
 
         #reversing for ease of indexing ('0' digit is leftmost)
         n_list.reverse()
-        print "is_sorted_original:", self.is_sorted_original(n_list)
+        #print "is_sorted_original:", self.is_sorted_original(n_list)
 
         if (self.is_sorted_original(n_list)):
             return -1
@@ -20,21 +20,23 @@ class NextHighestAnagram(object):
         #pointers signify which digit (list index) is currently being examined
         pointer1 = 0
         pointer2 = 1
-        self.main_loop(n_list, pointer1, pointer2)
+        return self.main_loop(n_list, pointer1, pointer2)
 
     def main_loop(self, n_list, pointer1, pointer2):
         while(True):
-            print "is_next_digit_lesser_value__pointer2:", self.is_next_digit_lesser_value__pointer2(n_list, pointer1, pointer2)
+            #print "is_next_digit_lesser_value__pointer2:", self.is_next_digit_lesser_value__pointer2(n_list, pointer1, pointer2)
             if self.is_next_digit_lesser_value__pointer2(n_list, pointer1, pointer2):
-                self.swap_split_sort_combine(n_list, pointer1, pointer2)
+                result = self.swap_split_sort_combine(n_list, pointer1, pointer2)
+                #print "RESULT: ",result
+                return result
 
-            print "is_pointer2_on_highest_digit:", self.is_pointer2_on_highest_digit(n_list, pointer2)
+            #print "is_pointer2_on_highest_digit:", self.is_pointer2_on_highest_digit(n_list, pointer2)
             if self.is_pointer2_on_highest_digit(n_list, pointer2):
-                print "move_pointer1_up_one_digit__pointer2_one_higher(output):", self.move_pointer1_up_one_digit__pointer2_one_higher(pointer1)
+                #print "move_pointer1_up_one_digit__pointer2_one_higher(output):", self.move_pointer1_up_one_digit__pointer2_one_higher(pointer1)
                 pointer1, pointer2 = self.move_pointer1_up_one_digit__pointer2_one_higher(pointer1)
-                self.main_loop(n_list, pointer1, pointer2)
+                return self.main_loop(n_list, pointer1, pointer2)
 
-            print "move_pointer2_up_one_digit(output):", self.move_pointer2_up_one_digit(pointer2)
+            #print "move_pointer2_up_one_digit(output):", self.move_pointer2_up_one_digit(pointer2)
             pointer2 = self.move_pointer2_up_one_digit(pointer2)
 
 
@@ -49,8 +51,8 @@ class NextHighestAnagram(object):
         return n_list[pointer2] < n_list[pointer1]
 
     def is_pointer2_on_highest_digit(self, n_list, pointer2):
-        print "len(n_list): ",len(n_list)-1
-        print "pointer2: ",pointer2
+        #print "len(n_list): ",len(n_list)-1
+        #print "pointer2: ",pointer2
         return pointer2 == len(n_list)-1
 
     def move_pointer2_up_one_digit(self, pointer2):
@@ -60,24 +62,25 @@ class NextHighestAnagram(object):
         return pointer1 + 1, pointer1 + 2
 
     def swap_split_sort_combine(self, n_list, pointer1, pointer2):
-        print "swap_split_sort_combine"
-        print n_list, pointer1, pointer2
+        #print "swap_split_sort_combine"
+        #print n_list, pointer1, pointer2
         #swap
         n_list[pointer1],n_list[pointer2]=n_list[pointer2],n_list[pointer1]
         lower_digits_list = n_list[:pointer2]
-        print lower_digits_list
+        #print lower_digits_list
         lower_digits_list.sort()
         lower_digits_list.reverse()
         return_list = lower_digits_list + n_list[pointer2:]
         return_list.reverse()
-        print "return_list:",return_list
-        result=int(''.join(return_list))
-        print result
-        exit()
+        #print "return_list:",return_list
+        return int(''.join(return_list))
+
 
 
 
 
 def next_bigger(n):
     nha = NextHighestAnagram()
-    return nha.go(n)
+    result = nha.go(n)
+    #print result
+    return result
