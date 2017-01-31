6d54a7941e12657be83b96b13176ccf51412d51f
Gehr <TEVIS.GEHR@accudyneindustries.com>
2017-01-31 10:28:51 -0700

    working main loop'

--- cw14.py
+++ cw14.py
@@ -2,28 +2,42 @@
 cd '.\Python\TevisPrograms\Code Wars'
 python
 import cw14 as cw14
-cw14.next_bigger(1213)
+cw14.next_bigger(5451)
 '''
 class NextHighestAnagram(object):
 
     def go(self, n):
         n_list = list(str(n))
         print n_list
+
+        #reversing for ease of indexing ('0' digit is leftmost)
+        n_list.reverse()
         print "is_sorted_original:", self.is_sorted_original(n_list)
 
         if (self.is_sorted_original(n_list)):
             return -1
 
-        pointer1 = 1
-        pointer2 = 2
+        #pointers signify which digit (list index) is currently being examined
+        pointer1 = 0
+        pointer2 = 1
+        self.main_loop(n_list, pointer1, pointer2)
 
-        print "is_next_digit_lesser_value__pointer2:", self.is_next_digit_lesser_value__pointer2(n_list, pointer1, pointer2)
-        if self.is_next_digit_lesser_value__pointer2(n_list, pointer1, pointer2):
-            pass
+    def main_loop(self, n_list, pointer1, pointer2):
+        while(True):
+            print "is_next_digit_lesser_value__pointer2:", self.is_next_digit_lesser_value__pointer2(n_list, pointer1, pointer2)
+            if self.is_next_digit_lesser_value__pointer2(n_list, pointer1, pointer2):
+                self.swap_split_sort_combine(n_list, pointer1, pointer2)
 
-        print "is_pointer2_on_highest_digit:", self.is_pointer2_on_highest_digit(n_list, pointer2)
-        if self.is_pointer2_on_highest_digit(n_list, pointer2):
-            pass
+            print "is_pointer2_on_highest_digit:", self.is_pointer2_on_highest_digit(n_list, pointer2)
+            if self.is_pointer2_on_highest_digit(n_list, pointer2):
+                print "move_pointer1_up_one_digit__pointer2_one_higher(output):", self.move_pointer1_up_one_digit__pointer2_one_higher(pointer1)
+                pointer1, pointer2 = self.move_pointer1_up_one_digit__pointer2_one_higher(pointer1)
+                self.main_loop(n_list, pointer1, pointer2)
+
+            print "move_pointer2_up_one_digit(output):", self.move_pointer2_up_one_digit(pointer2)
+            pointer2 = self.move_pointer2_up_one_digit(pointer2)
+
+
 
     def is_sorted_original(self,n_list):
         if (sorted(n_list) == n_list):
@@ -35,17 +49,22 @@ class NextHighestAnagram(object):
         return n_list[pointer2] < n_list[pointer1]
 
     def is_pointer2_on_highest_digit(self, n_list, pointer2):
-        return pointer2 == len(n_list)+1
+        print "len(n_list): ",len(n_list)-1
+        print "pointer2: ",pointer2
+        return pointer2 == len(n_list)-1
 
-    def move_pointer2_up_one_digit(self):
-        pass
+    def move_pointer2_up_one_digit(self, pointer2):
+        return pointer2 + 1
 
-    def move_pointer1_up_one_digit__pointer2_one_higher(self):
-        pass
+    def move_pointer1_up_one_digit__pointer2_one_higher(self, pointer1):
+        return pointer1 + 1, pointer1 + 2
 
-    def swap_split_sort_combine(self):
-        pass
+    def swap_split_sort_combine(self, n_list, pointer1, pointer2):
+        print "swap_split_sort_combine"
+        print n_list, pointer1, pointer2
 
+        #dont forget to n_list.reverse()
+        exit()
 
 
 
